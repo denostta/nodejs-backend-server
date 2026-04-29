23722bafd415443cae770ee46566fe303f03e637
denostta <denostaanthony08@gmail.com>
2026-04-29 09:20:40 +0300

    fixed the port

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import { connectDB, disconnectDB } from './config/db.js'
 config()
 connectDB()
 const app = express()
-const PORT = 5001
+const PORT = process.env.PORT || 5001
 //middleware for body parsing
 app.use(express.json())
 
