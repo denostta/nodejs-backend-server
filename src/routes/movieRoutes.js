9c4c38f7c9bdb99b528bf8527e00ce0db0ddda69
denostta <denostaanthony08@gmail.com>
2026-04-29 11:38:52 +0300

    test updating the images and updating the render server

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -3,16 +3,16 @@ import express from 'express'
 const router = express.Router()
 
 router.get("/hello", (req, res) => {
-    res.json({msg: "get movie route"})
+    res.json({msg: "get movie route UPDATED"})
 })
 router.post("/hello", (req, res) => {
-    res.json({msg: "post movie route"})
+    res.json({msg: "post movie route UPDATED"})
 })
 router.put("/hello", (req, res) => {
-    res.json({msg: "put  movie route"})
+    res.json({msg: "put  movie route UPDATED"})
 })
 router.delete("/hello", (req, res) => {
-    res.json({msg: "delete movie route"})
+    res.json({msg: "delete movie route UPDATED"})
 })
 
 
